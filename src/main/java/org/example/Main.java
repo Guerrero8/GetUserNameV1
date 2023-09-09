5660b43200653fc24b787c5ce871a5d3503a01cf
Guerrero8 <igo2rlol@gmail.com>
2023-09-09 16:37:34 +0300

    added filtration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.entity.Person;
 import org.example.service.PersonService;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +15,12 @@ public class Main {
         person.setUserLogin(scanner.nextLine());
         System.out.println(person);
         PersonService personService = new PersonService();
-        personService.generateFakePersons();
+        List<Person> people = personService.generateFakePersons();
+        List<Person> filteredFakePersonsOnSalary = personService.getFilteredFakePersonsOnSalary(people, 10000000);
+        int counter = 1;
+        for (Person filteredPerson: filteredFakePersonsOnSalary) {
+            System.out.println(counter + " " + filteredPerson + "\n");
+            counter++;
         }
     }
+}
--- src/main/java/org/example/service/PersonService.java
+++ src/main/java/org/example/service/PersonService.java
@@ -5,23 +5,35 @@ import org.example.entity.Person;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class PersonService {
-    public void generateFakePersons() {
+    public List<Person> generateFakePersons() {
         List<Person> persons = new ArrayList<>();
+        Faker faker = new Faker();
         for (int i = 0; i < 1000; i++) {
-            Faker faker = new Faker();
             Person person = new Person();
             person.setUserName(faker.name().fullName());
             person.setUserLogin(faker.leagueOfLegends().champion());
+            person.setSalary(RandomGenerator.getDefault().nextLong());
             persons.add(person);
         }
         int counter = 1;
         for (Person person : persons) {
             System.out.println(counter + " " + person + "\n");
             counter++;
-
         }
+        return persons;
+    }
+    public List<Person> getFilteredFakePersonsOnSalary(List<Person> persons, long minSalary) {
+        List<Person> filteredPersons = new ArrayList<>();
+        for (Person person: persons){
+            if (person.getSalary() > minSalary){
+                filteredPersons.add(person);
+            }
+        }
+        return filteredPersons;
     }
 
 }
