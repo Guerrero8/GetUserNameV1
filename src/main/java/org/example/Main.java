8585c60dc928c00d1c280d62582784418ef51a37
Guerrero8 <igo2rlol@gmail.com>
2023-11-06 16:58:42 +0300

    added mysql databaze and migraton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,25 +2,28 @@ package org.example;
 
 import org.example.entity.Person;
 import org.example.service.PersonService;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
 
         Scanner scanner = new Scanner(System.in);
         Person person = new Person();
-       /** person.setUserName(scanner.nextLine());
-        person.setUserLogin(scanner.nextLine()); **/
+        person.setUserName(scanner.nextLine());
+        person.setUserLogin(scanner.nextLine());
         System.out.println(person);
         PersonService personService = new PersonService();
         List<Person> people = personService.generateFakePersons();
 
         List<Person> filteredFakePersonsOnSalary = personService.getFilteredFakePersonsOnSalary(people, 10000000);
         int counter = 1;
-        for (Person filteredPerson: filteredFakePersonsOnSalary) {
+        for (Person filteredPerson : filteredFakePersonsOnSalary) {
             System.out.println(counter + " " + filteredPerson + "\n");
             counter++;
         }
@@ -31,5 +34,7 @@ public class Main {
             System.out.println(counter2 + " " + personFromApi + "\n");
             counter2++;
         }
+
     }
 }
+
